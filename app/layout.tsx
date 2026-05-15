25ee2893fc6487b238b8bc53e9802dad71e44220
BlazarQ <134844754+BlazarQ@users.noreply.github.com>
2026-05-15 12:52:14 +0800

    Update layout.tsx again

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css"
-import Sidebar from "@/components/ui/sidebar";
+import { Sidebar } from "@/components/ui/sidebar";
 import { Toaster } from "sonner";
 
 const geistSans = Geist({
